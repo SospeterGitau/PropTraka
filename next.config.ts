0ec061cc62131c39e57d1f483cdec66af9ed2461
Sospeter Gitau <sospeter.gitau@gmail.com>
2025-10-02 12:23:54 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: process.cwd(),
-  },
+  outputFileTracingRoot: process.cwd(),
   images: {
     remotePatterns: [
       {
