ae86fd389a69b25b015d22daa02da31bb4a8428b
Sospeter Gitau <sospeter.gitau@gmail.com>
2025-10-14 16:00:58 +0000

    I have been unable to publish again. Please review the fix bellow so tha

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,7 +1,4 @@
 
-import { config } from 'dotenv';
-config();
-
 import '@/ai/flows/generate-report-summary.ts';
 import '@/ai/flows/generate-pnl-report.ts';
 import '@/ai/flows/generate-market-research.ts';
