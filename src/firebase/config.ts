664016c74711083b675b26d343ee51db1f5c454e
Sospeter Gitau <sospeter.gitau@gmail.com>
2025-10-15 08:54:06 +0000

    I do not see the connections to the database or analytics.
    Here are a f

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,6 @@ export const firebaseConfig = {
   "appId": "1:1096299264561:web:70d7993ed8718e10f87310",
   "apiKey": "AIzaSyBK9LKO2xVWYWGdJ6OFWg4z5wYmJAV701I",
   "authDomain": "studio-6577669797-1b758.firebaseapp.com",
-  "measurementId": "",
+  "measurementId": "G-4402P52002",
   "messagingSenderId": "1096299264561"
 };
