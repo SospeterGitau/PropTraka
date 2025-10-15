2ff58ec9c84a5e79b186a6adf9e5f7b30dec59dd
Sospeter Gitau <sospeter.gitau@gmail.com>
2025-10-15 07:56:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6577669797-1b758",
+  "appId": "1:1096299264561:web:70d7993ed8718e10f87310",
+  "apiKey": "AIzaSyBK9LKO2xVWYWGdJ6OFWg4z5wYmJAV701I",
+  "authDomain": "studio-6577669797-1b758.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1096299264561"
+};
