1c4284407aed4e03ab7e182137ad4af3a759d253
Sospeter Gitau <sospeter.gitau@gmail.com>
2025-10-16 14:49:08 +0000

    404 error

--- src/app/(dashboard/reports)/page.tsx
+++ src/app/(dashboard/reports)/page.tsx
@@ -1,7 +1,7 @@
 // Server file: this is where Next will actually read the route options.
 export const dynamic = 'force-dynamic';
 
-import ReportsClient from "./ReportsClient";
+import ReportsClient from "./reports-client";
 
 export default function Page() {
   return <ReportsClient />;
