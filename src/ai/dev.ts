bada61e345a248c53a06cf89887a6447f6ecba3d
Sospeter Gitau <sospeter.gitau@gmail.com>
2025-11-11 12:08:07 +0000

    is it that I need to log in to the live version of the app to make the c

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,4 +1,11 @@
 
+import {initializeApp, applicationDefault} from 'firebase-admin/app';
+
+initializeApp({
+    credential: applicationDefault(),
+    projectId: process.env.FIREBASE_PROJECT_ID,
+});
+
 import '@/ai/flows/generate-report-summary.ts';
 import '@/ai/flows/generate-pnl-report.ts';
 import '@/ai/flows/generate-market-research.ts';
