6954bd04508cce0b91b6a592ecbcd13c1c3d0f78
Sospeter Gitau <sospeter.gitau@gmail.com>
2025-11-13 16:22:28 +0000

    the bubble NEEDS TO BE ON THE BOTTOM RIGHT HAD SIDE!!! please tel me tha

--- src/components/chat-bubble.tsx
+++ src/components/chat-bubble.tsx
@@ -10,7 +10,7 @@ export function ChatBubble() {
 
   return (
     <>
-      <div className="fixed bottom-6 left-6 z-50">
+      <div className="fixed bottom-6 right-6 z-50">
         <Button
           size="icon"
           className="rounded-full w-14 h-14 shadow-lg"
