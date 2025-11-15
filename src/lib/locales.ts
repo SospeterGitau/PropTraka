a3a18c63bf5a4863a052b335ef9a5dcb7166ba01
Sospeter Gitau <sospeter.gitau@gmail.com>
2025-11-15 14:05:13 +0000

    Fix the date-fns locale import error in lib/locales.ts line 7.
    ERROR:

--- src/lib/locales.ts
+++ src/lib/locales.ts
@@ -4,15 +4,15 @@ import type { Locale } from 'date-fns';
 export async function getLocale(locale: string): Promise<Locale> {
   switch (locale) {
     case 'en-GB':
-      return (await import('date-fns/locale/en-GB')).default;
+      return (await import('date-fns/locale/en-GB')).enGB;
     case 'de-DE':
-      return (await import('date-fns/locale/de')).default;
+      return (await import('date-fns/locale/de')).de;
     case 'fr-FR':
-      return (await import('date-fns/locale/fr')).default;
+      return (await import('date-fns/locale/fr')).fr;
     case 'ja-JP':
-        return (await import('date-fns/locale/ja')).default;
+        return (await import('date-fns/locale/ja')).ja;
     case 'en-US':
     default:
-      return (await import('date-fns/locale/en-US')).default;
+      return (await import('date-fns/locale/en-US')).enUS;
   }
 }
