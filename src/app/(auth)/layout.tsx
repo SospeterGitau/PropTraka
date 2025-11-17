b67fd1e511fce6e62a095a879e315a0e0207eb47
Sospeter Gitau <sospeter.gitau@gmail.com>
2025-11-17 13:45:19 +0000

    I found the solution in the Next.js documentation and GitHub issues! The

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,18 +1,11 @@
-
-'use client';
-
-import { FirebaseClientProvider } from "@/firebase/client-provider";
-
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <FirebaseClientProvider>
-        <div className="flex h-screen w-full items-center justify-center bg-muted/40 p-4">
-            {children}
-        </div>
-    </FirebaseClientProvider>
+    <div className="flex h-screen w-full items-center justify-center bg-muted/40 p-4">
+        {children}
+    </div>
   );
 }
