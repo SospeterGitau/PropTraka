2069aa0e901c557307676248439cac3be5c6beed
Sospeter Gitau <sospeter.gitau@gmail.com>
2025-11-23 17:54:43 +0000

    Try fixing this error: `Runtime Error: Error: useFirebase is not defined

--- src/components/settings/profile-settings-tab.tsx
+++ src/components/settings/profile-settings-tab.tsx
@@ -7,7 +7,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 
-import { useUser, useFirestore } from '@/firebase';
+import { useUser, useFirestore, useFirebase } from '@/firebase';
 import { useTheme } from '@/context/theme-context';
 import { useDataContext } from '@/context/data-context';
 import { useToast } from '@/hooks/use-toast';
